678889e173b23b7d79789844201ce0ad5ea26b54
miseo_jeon <83704225+raichal2000@users.noreply.github.com>
2023-06-23 16:03:11 +0900

    merge

--- breakout.py
+++ breakout.py
@@ -25,6 +25,111 @@
 
 import math,pygame,sys,shutil,getpass
 from pygame.locals import *
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import random
+from collections import deque
+from torchvision.transforms import Compose, Grayscale, Resize, ToTensor
+
+# Replay memory capacity
+N = 1000
+
+# Exploration rate (epsilon)
+epsilon = 1.0
+epsilon_decay = 0.99
+epsilon_min = 0.01
+
+# Discount factor (gamma)
+gamma = 0.99
+
+# Define Q-network
+class QNetwork(nn.Module):
+    def __init__(self, input_size, output_size):
+        super(QNetwork, self).__init__()
+        self.fc1 = nn.Linear(input_size, 24)
+        self.fc2 = nn.Linear(24, 24)
+        self.fc3 = nn.Linear(24, output_size)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        return self.fc3(x)
+
+# Initialize replay memory D
+D = deque(maxlen=N)
+
+# Preprocess state function
+preprocess = Compose([
+    Grayscale(),
+    Resize((81, 81)),
+    ToTensor()
+])
+
+# Episode and time steps
+M = 100
+T = 100
+
+# Define action and state sizes
+action_size = 9
+state_size = 81
+
+# Create Q-network
+Q = QNetwork(state_size, action_size)
+Q_target = QNetwork(state_size, action_size)
+optimizer = optim.Adam(Q.parameters(), lr=0.001)
+
+# Move Q-network to GPU if available
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+Q.to(device)
+Q_target.to(device)
+
+# Copy weights from Q to Q_target
+Q_target.load_state_dict(Q.state_dict())
+
+def select_action(state):
+    if random.random() <= epsilon:
+        return random.randint(0, action_size - 1)
+    else:
+        state = torch.tensor(state).unsqueeze(0).to(device)
+        with torch.no_grad():
+            q_values = Q(state)
+        return torch.argmax(q_values).item()
+
+def update_Q_network():
+    if len(D) < batch_size:
+        return
+
+    # Sample random minibatch from replay memory
+    minibatch = random.sample(D, batch_size)
+    states, actions, rewards, next_states = zip(*minibatch)
+
+    states = torch.tensor(states).to(device)
+    actions = torch.tensor(actions).unsqueeze(1).to(device)
+    rewards = torch.tensor(rewards).to(device)
+    next_states = torch.tensor(next_states).to(device)
+
+    # Calculate target Q-values
+    with torch.no_grad():
+        q_values_next = Q_target(next_states).max(1)[0]
+        targets = rewards + gamma * q_values_next
+
+    # Calculate current Q-values
+    q_values = Q(states).gather(1, actions)
+
+    # Update Q-network
+    loss = F.mse_loss(q_values, targets.unsqueeze(1))
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    # Update target network
+    Q_target.load_state_dict(Q.state_dict())
+
+# Initialize sequence s1 = [1]
+s = [1]
+
 
 pygame.init()
 fpsClock = pygame.time.Clock()
@@ -83,13 +188,12 @@ class Paddle: #class for paddle vars
 class Ball: #class for ball vars
     x = 0
     y = 0
-    remaining = 3
+    remaining = 1
     xPos = 1 #amount increasing by for x. adjusted for speed
     yPos = 1
     adjusted = False #says wether the xPos and yPos have been adjusted for speed
     speed = 5
     collisions = 0
-    alive = False
     moving = False
     def adjust(self): #adjusts the x and y being added to the ball to make the hypotenuse the ball speed
         tSlope = math.sqrt(self.xPos**2 + self.yPos**2)
@@ -249,10 +353,11 @@ def game(score,paddle,ball,board,wall1): #The game itself
                         paddle.direction = 'none'
           
         #update display
-        pygame.image.save(screen,'temp.jpeg') 
         pygame.display.update()
-       
-        fpsClock.tick(30)
+        pixels = pygame.surfarray.array3d(screen)
+        tensor = torch.from_numpy(pixels)
+        #print(tensor.shape)
+        fpsClock.tick(50)
     return score
 def get_highscore(score):
     place = 20
@@ -439,6 +544,7 @@ if __name__ == '__main__':
             ball = Ball()
             while ball.remaining > 0:
                 score = game(score,paddle,ball,board,wall1)
+                
                 if ball.remaining == 0:
                     for x in range(16):
                         for y in range(12):
@@ -455,6 +561,7 @@ if __name__ == '__main__':
                         board = new_board()
                         while ball.remaining > 0:
                             score = game(score,paddle,ball,board,wall1)
+                            print(score)
                             if ball.remaining == 0:
                                 for x in range(16):
                                     for y in range(12):
